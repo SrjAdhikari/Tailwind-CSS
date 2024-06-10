fc8cc987bb00341abd7731dc81f6e752537b61ea
SrjAdhikari <surajadhikari055@gmail.com>
2024-06-10 14:17:38 +0900

    Learning Tailwind CSS

---
+++ src/components/Nav.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+const Nav = () => {
+    return (
+        <header>
+            <nav>
+                <a href="/">
+                <img src="" alt="" /></a>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav
